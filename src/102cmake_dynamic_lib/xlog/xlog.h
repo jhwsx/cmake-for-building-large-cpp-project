8f25c963a9716835abddb99b8703ab3d86741dd7
wangzhichao <wangzhichao@abupdate.com>
2024-02-22 07:51:51 +0800

    102cmake_lib build and link dynamic lib for windows

--- src/102cmake_dynamic_lib/xlog/xlog.h
+++ src/102cmake_dynamic_lib/xlog/xlog.h
@@ -1,7 +1,21 @@
 #ifndef XLOG_H
 #define XLOG_H
 
-class XLog
+#ifndef _WIN32
+    #define XCPP_API
+#else
+    // windows encoding use utf-8 with BOM
+    // __declspec(dllexport) 到处 XLog 类 的函数到 lib 文件中
+    // xlog 库文件调用时，做 dllexport
+    // test_xlog 库文件调用时，做 dllimport
+    #ifdef XLOG_EXPORTS
+        #define XCPP_API __declspec(dllexport)
+    #else
+        #define XCPP_API __declspec(dllimport)
+    #endif
+#endif // for _WIN32
+
+class XCPP_API XLog
 {
 public:
     XLog();
