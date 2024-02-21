7a38c40fa03eb3d3c2226df24a22ce7324be96b7
wangzhichao <wangzhichao@abupdate.com>
2024-02-22 00:02:11 +0800

    102cmake_lib build and link dynamic lib

---
+++ src/102cmake_dynamic_lib/test_xlog/test_xlog.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "xlog.h"
+
+int main(int argc, char const *argv[])
+{
+    XLog xlog;
+    std::cout << "test xlog\n";
+
+    return 0;
+}
---
+++ src/102cmake_dynamic_lib/xlog/xlog.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "xlog.h"
+
+XLog::XLog()
+{
+    std::cout << "XLog::XLog()\n";
+}
