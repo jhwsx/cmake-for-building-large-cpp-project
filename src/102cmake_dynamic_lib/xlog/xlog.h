615e2b59551bd8d171ccae19599f5110bb971072
wangzhichao <wangzhichao@abupdate.com>
2024-02-24 10:13:41 +0800

    106cmake_system_ver 4 cmake system variables

--- src/102cmake_dynamic_lib/xlog/xlog.h
+++ src/102cmake_dynamic_lib/xlog/xlog.h
@@ -8,7 +8,7 @@
     // __declspec(dllexport) 到处 XLog 类 的函数到 lib 文件中
     // xlog 库文件调用时，做 dllexport
     // test_xlog 库文件调用时，做 dllimport
-    #ifdef XLOG_EXPORTS
+    #ifdef xlog_EXPORTS
         #define XCPP_API __declspec(dllexport)
     #else
         #define XCPP_API __declspec(dllimport)
