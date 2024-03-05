651bd1486574f24a7f2d615cea79e1009087ce11
wangzhichao <wangzhichao@abupdate.com>
2024-03-06 07:25:50 +0800

    203cmake_property: target property

--- src/203cmake_property/main.cpp
+++ src/203cmake_property/main.cpp
@@ -3,6 +3,24 @@
 int main(int argc, char const *argv[])
 {
     std::cout << "test cmake property\n";
+#ifdef PARA1
     std::cout << "PARA1 = " << PARA1 << '\n';
+#endif
+    
+#ifdef PARA2
+    std::cout << "PARA2 = " << PARA2 << '\n';
+#endif
+
+#ifdef PARA3
+    std::cout << "PARA3 = " << PARA3 << '\n';
+#endif
+       
+#ifdef PARA4
+    std::cout << "PARA4 = " << PARA4 << '\n';
+#endif
+
+#ifdef PARA5
+    std::cout << "PARA5 = " << PARA5 << '\n';
+#endif
     return 0;
 }
