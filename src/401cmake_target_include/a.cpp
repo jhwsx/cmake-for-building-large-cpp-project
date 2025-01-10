62c9f3f1a43613e4272d7fdefc5b0932d78416e0
wangzhichao <wangzhichao@abupdate.com>
2025-01-10 21:30:11 +0800

    401cmake_target_include: target_compile_definitions review

--- src/401cmake_target_include/a.cpp
+++ src/401cmake_target_include/a.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
 void A() 
 {
+#ifdef VAR_A
+    std::cout << "in A: " << VAR_A << std::endl;
+#else
+    std::cout << "in A: VAR_A is not defined\n";
+#endif
 }
--- src/401cmake_target_include/b.cpp
+++ src/401cmake_target_include/b.cpp
@@ -2,4 +2,9 @@
 void A();
 void B(){
     A();
+#ifdef VAR_A
+    std::cout << "in B: " << VAR_A << std::endl;
+#else
+    std::cout << "in B: VAR_A is not defined\n";
+#endif
 }
